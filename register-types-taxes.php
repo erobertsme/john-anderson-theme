901665df1dfd1a294d5400b0af949e433e79d95e
Ethan Roberts <spaminol@gmail.com>
2021-03-08 16:20:43 -0800

    Fix indenting

--- register-types-taxes.php
+++ register-types-taxes.php
@@ -47,83 +47,83 @@ add_action( 'init', 'register_tribes' );
 
 function register_tribe_taxonomies() {
 
-	/**
-	 * Taxonomy: Tribes.
-	 */
+  /**
+   * Taxonomy: Tribes.
+   */
 
-	$labels = [
-		"name" => __( "Tribes" ),
-		"singular_name" => __( "Tribe" ),
-	];
-
-	$args = [
-		"label" => __( "Tribes" ),
-		"labels" => $labels,
-		"public" => true,
-		"publicly_queryable" => true,
-		"hierarchical" => false,
-		"show_ui" => true,
-		"show_in_menu" => true,
-		"show_in_nav_menus" => true,
-		"query_var" => true,
-		"rewrite" => [ 'slug' => 'tribes', 'with_front' => true, ],
-		"show_admin_column" => true,
-		"show_in_rest" => true,
-		"rest_base" => "tribes",
-		"rest_controller_class" => "WP_REST_Terms_Controller",
-		"show_in_quick_edit" => true,
-		"meta_box_cb" => false,
+  $labels = [
+    "name" => __( "Tribes" ),
+    "singular_name" => __( "Tribe" ),
   ];
 
-	register_taxonomy( "tribes", [ "tribe" ], $args );
-
-	/**
-	 * Taxonomy: Tribe Categories.
-	 */
-
-	$labels = [
-		"name" => __( "Tribe Categories" ),
-		"singular_name" => __( "Tribe Category" ),
-		"menu_name" => __( "Tribe Category" ),
-		"all_items" => __( "All Tribe Categories" ),
-		"edit_item" => __( "Edit Tribe Category" ),
-		"view_item" => __( "View Tribe Category" ),
-		"update_item" => __( "Update Tribe Category name" ),
-		"add_new_item" => __( "Add new Tribe Category" ),
-		"new_item_name" => __( "New Tribe Category name" ),
-		"parent_item" => __( "Parent Tribe Category" ),
-		"parent_item_colon" => __( "Parent Tribe Category:" ),
-		"search_items" => __( "Search Tribe Categories" ),
-		"popular_items" => __( "Popular Tribe Categories" ),
-		"separate_items_with_commas" => __( "Separate Tribe Category with commas" ),
-		"add_or_remove_items" => __( "Add or remove Tribe Category" ),
-		"choose_from_most_used" => __( "Choose from the most used Tribe Categories" ),
-		"not_found" => __( "No Tribe Category found" ),
-		"no_terms" => __( "No Tribe Category" ),
-		"items_list_navigation" => __( "Tribe Category list navigation" ),
-		"items_list" => __( "Tribe Category list" ),
-		"back_to_items" => __( "Back to Tribe Categories" ),
-	];
-
-	$args = [
-		"label" => __( "Tribe Categories" ),
-		"labels" => $labels,
-		"public" => true,
-		"publicly_queryable" => true,
-		"hierarchical" => false,
-		"show_ui" => true,
-		"show_in_menu" => true,
-		"show_in_nav_menus" => true,
-		"query_var" => true,
-		"rewrite" => [ 'slug' => 'cat', 'with_front' => true, ],
-		"show_admin_column" => false,
-		"show_in_rest" => true,
-		"rest_base" => "cat",
-		"rest_controller_class" => "WP_REST_Terms_Controller",
-		"show_in_quick_edit" => false,
-		"meta_box_cb" => false,
+  $args = [
+    "label" => __( "Tribes" ),
+    "labels" => $labels,
+    "public" => true,
+    "publicly_queryable" => true,
+    "hierarchical" => false,
+    "show_ui" => true,
+    "show_in_menu" => true,
+    "show_in_nav_menus" => true,
+    "query_var" => true,
+    "rewrite" => [ 'slug' => 'tribes', 'with_front' => true, ],
+    "show_admin_column" => true,
+    "show_in_rest" => true,
+    "rest_base" => "tribes",
+    "rest_controller_class" => "WP_REST_Terms_Controller",
+    "show_in_quick_edit" => true,
+    "meta_box_cb" => false,
   ];
 
-	register_taxonomy( "tribe_category", [ "tribe" ], $args );
+  register_taxonomy( "tribes", [ "tribe" ], $args );
+
+  /**
+   * Taxonomy: Tribe Categories.
+   */
+
+  $labels = [
+    "name" => __( "Tribe Categories" ),
+    "singular_name" => __( "Tribe Category" ),
+    "menu_name" => __( "Tribe Category" ),
+    "all_items" => __( "All Tribe Categories" ),
+    "edit_item" => __( "Edit Tribe Category" ),
+    "view_item" => __( "View Tribe Category" ),
+    "update_item" => __( "Update Tribe Category name" ),
+    "add_new_item" => __( "Add new Tribe Category" ),
+    "new_item_name" => __( "New Tribe Category name" ),
+    "parent_item" => __( "Parent Tribe Category" ),
+    "parent_item_colon" => __( "Parent Tribe Category:" ),
+    "search_items" => __( "Search Tribe Categories" ),
+    "popular_items" => __( "Popular Tribe Categories" ),
+    "separate_items_with_commas" => __( "Separate Tribe Category with commas" ),
+    "add_or_remove_items" => __( "Add or remove Tribe Category" ),
+    "choose_from_most_used" => __( "Choose from the most used Tribe Categories" ),
+    "not_found" => __( "No Tribe Category found" ),
+    "no_terms" => __( "No Tribe Category" ),
+    "items_list_navigation" => __( "Tribe Category list navigation" ),
+    "items_list" => __( "Tribe Category list" ),
+    "back_to_items" => __( "Back to Tribe Categories" ),
+  ];
+
+  $args = [
+    "label" => __( "Tribe Categories" ),
+    "labels" => $labels,
+    "public" => true,
+    "publicly_queryable" => true,
+    "hierarchical" => false,
+    "show_ui" => true,
+    "show_in_menu" => true,
+    "show_in_nav_menus" => true,
+    "query_var" => true,
+    "rewrite" => [ 'slug' => 'cat', 'with_front' => true, ],
+    "show_admin_column" => false,
+    "show_in_rest" => true,
+    "rest_base" => "cat",
+    "rest_controller_class" => "WP_REST_Terms_Controller",
+    "show_in_quick_edit" => false,
+    "meta_box_cb" => false,
+  ];
+
+  register_taxonomy( "tribe_category", [ "tribe" ], $args );
 }
 add_action( 'init', 'register_tribe_taxonomies' );
